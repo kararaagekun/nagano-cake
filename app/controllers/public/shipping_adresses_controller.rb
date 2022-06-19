2a92a73a4c823d3122f98d1b61e1a646777d742d
EC2 Default User <kakaloto0824@gmail.com>
2022-06-19 11:57:30 +0000

    Add controller orders order_products shipping_adresses homes

---
+++ app/controllers/public/shipping_adresses_controller.rb
@@ -0,0 +1,2 @@
+class Public::ShippingAdressesController < ApplicationController
+end
---
+++ test/controllers/public/shipping_adresses_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Public::ShippingAdressesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
