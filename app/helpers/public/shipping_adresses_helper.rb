9a96505c837a11728963f6f3edc8b9f7e2f85a2d
EC2 Default User <kakaloto0824@gmail.com>
2022-06-20 01:01:42 +0000

    Add controller

--- app/helpers/public/shipping_adresses_helper.rb
+++
@@ -1,2 +0,0 @@
-module Public::ShippingAdressesHelper
-end
--- test/controllers/public/shipping_adresses_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Public::ShippingAdressesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
